1873cfc00d946c2bf5539dd0f7451571282d75f0
umagnanasundaram2128 <guma1995@gmail.com>
2023-07-05 02:35:44 -0400

    Added - doctor registration form UI

--- frontend/src/RegistrationForm.js
+++ frontend/src/RegistrationForm.js
@@ -2,6 +2,8 @@ import React, { useState } from 'react';
 import './RegistrationForm.css'; // Import the CSS file for styling
 
 const RegistrationForm = () => {
+
+
   const [email, setEmail] = useState('');
   const [userType, setUserType] = useState('');
   const [name, setName] = useState('');
@@ -14,8 +16,33 @@ const RegistrationForm = () => {
   const [password, setPassword] = useState('');
   const [submissionStatus, setSubmissionStatus] = useState('');
 
+  const [doctorValues, setDoctorValues] = useState({
+    firstName: '',
+    lastName: '',
+    userName: '',
+    department: '',
+    dateOfBirth: '',
+    age:'',
+    email:'',
+    addressLine1: '',
+    addressLine2: '',
+    state: '',
+    city: '',
+    country: '',
+    zipCode: '',
+    createdAt: ''
+  });
+
   const handleUserTypeChange = (e) => {
     setUserType(e.target.value);
+  };
+
+  const handleDoctorFormChange = (e) => {
+    const { name, value } = e.target;
+    setDoctorValues((prevValues) => ({
+      ...prevValues,
+      [name]: value,
+    }));
   };
 
   const handleSubmit = (e) => {
@@ -116,6 +143,115 @@ const RegistrationForm = () => {
               <label htmlFor="password">Password:</label>
               <input type="password" id="password" value={password} onChange={(e) => setPassword(e.target.value)} required />
             </div>
+          </div>
+        )}
+        {userType === 'doctor' && (
+          <div className="user-details">
+            <div className="form-group">
+              <label htmlFor="fName">First Name:</label>
+              <input type="text" id="fname" value={doctorValues.firstName} onChange={handleDoctorFormChange} required />
+            </div>
+            <div className="form-group">
+              <label htmlFor="lName">Last Name:</label>
+              <input type="text" id="lname" value={doctorValues.LastName} onChange={handleDoctorFormChange} required />
+            </div>
+            <div className="form-group">
+              <label htmlFor="uName">First Name:</label>
+              <input type="text" id="uname" value={doctorValues.userName} onChange={handleDoctorFormChange} required />
+            </div>
+            <div className="form-group">
+              <label htmlFor="department">Department:</label>
+              <input type="text" id="departmentName" value={doctorValues.department} onChange={handleDoctorFormChange} required />
+            </div>
+            <div className="form-group">
+              <label htmlFor="dob">Date Of Birth:</label>
+              <input type="text" id="dob" value={doctorValues.dateOfBirth} onChange={handleDoctorFormChange} required />
+            </div>
+            <div className="form-group">
+              <label htmlFor="age">Age:</label>
+              <input type="text" id="age" value={doctorValues.age} onChange={handleDoctorFormChange} required />
+            </div>
+            <div className="form-group">
+              <label htmlFor="email">Email:</label>
+              <input type="text" id="dEmail" value={doctorValues.email} onChange={handleDoctorFormChange} required />
+            </div>
+            <div className="form-group">
+              <label htmlFor="addressLine1">Address Line 1:</label>
+              <input type="text" id="docAddressLine1" value={doctorValues.addressLine1} onChange={handleDoctorFormChange} required />
+            </div>
+
+            <div className="form-group">
+              <label htmlFor="addressLine2">Address Line 2:</label>
+              <input type="text" id="docAddressLine2" value={doctorValues.addressLine2} onChange={handleDoctorFormChange} />
+            </div>
+
+            <div className="form-group">
+              <label htmlFor="state">State:</label>
+              <input type="text" id="docState" value={doctorValues.state} onChange={handleDoctorFormChange} required />
+            </div>
+
+            <div className="form-group">
+              <label htmlFor="city">City:</label>
+              <input type="text" id="docCity" value={doctorValues.city} onChange={handleDoctorFormChange} required />
+            </div>
+
+            <div className="form-group">
+              <label htmlFor="country">Country:</label>
+              <input type="text" id="docCountry" value={doctorValues.country} onChange={handleDoctorFormChange} required />
+            </div>
+
+            <div className="form-group">
+              <label htmlFor="zipCode">Zip Code:</label>
+              <input type="text" id="zipCode" value={doctorValues.zipCode} onChange={handleDoctorFormChange} required />
+            </div>
+
+            <div className="form-group">
+              <label htmlFor="createdAt">Created At:</label>
+              <input type="text" id="createdAt" value={doctorValues.createdAt} onChange={handleDoctorFormChange} required />
+            </div>
+
+          </div>
+        )}
+        {userType === 'department' && (
+          <div className="user-details">
+            <div className="form-group">
+              <label htmlFor="name">Department Name:</label>
+              <input type="text" id="dName" value={name} onChange={(e) => setName(e.target.value)} required />
+            </div>
+            <div className="form-group">
+              <label htmlFor="name">Department Email:</label>
+              <input type="email" id="dEmail" value={name} onChange={(e) => setName(e.target.value)} required />
+            </div>
+            <div className="form-group">
+              <label htmlFor="name">Department Name:</label>
+              <input type="text" id="name" value={name} onChange={(e) => setName(e.target.value)} required />
+            </div>
+            <div className="form-group">
+              <label htmlFor="name">Department Name:</label>
+              <input type="text" id="name" value={name} onChange={(e) => setName(e.target.value)} required />
+            </div>
+            <div className="form-group">
+              <label htmlFor="name">Department Name:</label>
+              <input type="text" id="name" value={name} onChange={(e) => setName(e.target.value)} required />
+            </div>
+            <div className="form-group">
+              <label htmlFor="name">Department Name:</label>
+              <input type="text" id="name" value={name} onChange={(e) => setName(e.target.value)} required />
+            </div>
+            <div className="form-group">
+              <label htmlFor="name">Department Name:</label>
+              <input type="text" id="name" value={name} onChange={(e) => setName(e.target.value)} required />
+            </div>
+
+          </div>
+        )}
+        {userType === 'labTech' && (
+          <div className="user-details">
+            <div className="form-group">
+              <label htmlFor="name">Name:</label>
+              <input type="text" id="name" value={name} onChange={(e) => setName(e.target.value)} required />
+            </div>
+
           </div>
         )}
 
